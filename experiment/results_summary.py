92aae5926d320d38669419b92233ba8efb313fad
MMedzin <michal.medzin@protonmail.com>
2023-05-29 14:00:59 +0200

    Prettier plots for final results

--- experiment/results_summary.py
+++ experiment/results_summary.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from typing import Optional
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import PathPatch
+from matplotlib.legend import _get_legend_handles_labels
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -36,75 +38,218 @@ DATASETS = {
     "METABRIC": load_metabric_data_normed(),
 }
 
-SCORES = [
-    ("silhouette", lambda x: max(x[~x.isna()], default=None)),
-    ("calinski_harabasz", lambda x: max(x[~x.isna()], default=None)),
-    ("davies_bouldin", lambda x: min(x[~x.isna()], default=None)),
-    ("adjusted_rand_index", lambda x: max(x[(x != 0) & (~x.isna())], default=None)),
-    ("adjusted_mutual_info", lambda x: max(x[(x > 0) & (~x.isna())], default=None)),
+SCORES = {
+    "silhouette": lambda x: max(x[~x.isna()], default=None),
+    "calinski_harabasz": lambda x: max(x[~x.isna()], default=None),
+    "davies_bouldin": lambda x: min(x[~x.isna()], default=None),
+    "adjusted_rand_index": lambda x: max(x[(x != 0) & (~x.isna())], default=None),
+    "adjusted_mutual_info": lambda x: max(x[(x > 0) & (~x.isna())], default=None),
+}
+
+SCORES_GROUPS = {
+    "unsupervised": ["silhouette", "calinski_harabasz", "davies_bouldin"],
+    "supervised": ["adjusted_rand_index", "adjusted_mutual_info"],
+}
+
+SCORES_PRETTY_NAMES = {
+    "silhouette": "Silhouette Coeff.",
+    "calinski_harabasz": "Calinski-Harabasz Index",
+    "davies_bouldin": "Davies-Bouldin Index",
+    "adjusted_rand_index": "Adjusted Rand Index",
+    "adjusted_mutual_info": "Adjusted Mutual Info.",
+}
+
+ALGO_GROUPS = {
+    "KMeans": "Partition-based",
+    "KMedoids": "Partition-based",
+    "AffinityPropagation": "Partition-based",
+    "AgglomerativeClustering": "Hierarchical",
+    "Birch": "Hierarchical",
+    "GaussianMixture": "Distribution-based",
+    "DBSCAN": "Density-based",
+    "OPTICS": "Density-based",
+    "SOM": "Model-based",
+    "SpectralClustering": "Graph-based",
+}
+
+ALGO_GROUPS_ORDER = [
+    "Partition-based",
+    "Hierarchical",
+    "Distribution-based",
+    "Density-based",
+    "Model-based",
+    "Graph-based",
 ]
+
+PALETTE = sns.color_palette()
+
+RESULTS_NAMES_DICT = {
+    "results_2023_05_23_1732_PCA_WHOLE_DATA": "PCA",
+    "results_2023_05_24_1020_StandardScaler_WHOLE_DATA": "StandardScaler",
+    "results_2023_05_24_1508_QuantileTransformer_WHOLE_DATA": "QuantileTransformer",
+    "results_2023_05_24_2340_MinMaxScaler_WHOLE_DATA": "MinMaxScaler",
+}
 
 
 def path_list_parse(arg: str, delim: str = ",") -> list[Path]:
     return [Path(a) for a in arg.split(delim)]
 
 
-def boxplot_mean_scores_per_algo(results_df: pd.DataFrame, save_path: Path) -> None:
-    n_cols = 3
-    n_rows = int(len(SCORES) // n_cols + 1 * (len(SCORES) % n_cols > 0))
-    fig = plt.figure(figsize=(n_rows * 10, n_cols * 5))
-    for n, (score, _) in enumerate(SCORES):
-        ax = fig.add_subplot(n_rows, n_cols, n + 1)
-        sns.boxplot(
-            data=results_df,
-            x="algo_name",
-            y=f"mean_test_{score}",
-            hue="results_tag"
-            if results_df.loc[:, "results_tag"].unique().shape[0] > 1
-            else None,
-            ax=ax,
+def boxplot_mean_scores_per_algo(
+    results_df: pd.DataFrame, save_path: Path, split_score_groups: bool = False
+) -> None:
+    if split_score_groups:
+        scores_groups = SCORES_GROUPS
+    else:
+        scores_groups = {"": list(SCORES.keys())}
+    for group_name, group_scores in scores_groups.items():
+        n_cols = 3 if len(group_scores) > 3 else len(group_scores)
+        n_rows = int(len(group_scores) // n_cols + 1 * (len(group_scores) % n_cols > 0))
+        fig = plt.figure(figsize=((n_cols + 1) * 5, n_rows * 5))
+        for n, score in enumerate(group_scores):
+            ax = fig.add_subplot(n_rows, n_cols, n + 1)
+            sns.boxplot(
+                data=results_df,
+                x="algo_name",
+                y=-1 * results_df.loc[:, f"mean_test_{score}"]
+                if score == "davies_bouldin"
+                else f"mean_test_{score}",
+                hue="results_tag"
+                if results_df.loc[:, "results_tag"].unique().shape[0] > 1
+                else None,
+                ax=ax,
+            )
+            ax.set_title(
+                ("-1 * " if score == "davies_bouldin" else "")
+                + SCORES_PRETTY_NAMES[score]
+            )
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+            box_patches = [patch for patch in ax.patches if type(patch) == PathPatch]
+            lines_per_boxplot = len(ax.lines) // len(box_patches)
+            for i, patch in enumerate(box_patches):
+                face_col = patch.get_facecolor()
+                patch.set_edgecolor(face_col)
+                for line in ax.lines[
+                    i * lines_per_boxplot : (i + 1) * lines_per_boxplot
+                ]:
+                    line.set_color(face_col)
+                    line.set_mfc(face_col)  # facecolor of fliers
+                    line.set_mec(face_col)  # edgecolor of fliers
+            ax.legend([], [], frameon=False)
+            ax.set_xlabel("")
+
+        fig.suptitle(
+            f"Mean{' ' + group_name if group_name != '' else ''} scores for differnt hyperparameters"
         )
-        ax.set_title(" ".join([s.capitalize() for s in score.split("_")]))
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
-    fig.suptitle("Mean scores for differnt hyperparameters")
-    plt.tight_layout()
-    fig.savefig(save_path)
+        plt.legend(loc="lower left", bbox_to_anchor=(1.05, 0.4))
+        plt.tight_layout()
+        fig.savefig(
+            save_path.parent
+            / f"{save_path.stem}{'_' + group_name if group_name != '' else ''}{save_path.suffix}"
+        )
 
 
-def boxplot_scores_per_best_config(results_df: pd.DataFrame, save_path: Path) -> None:
-    n_cols = 3
-    n_rows = int(len(SCORES) // n_cols + 1 * (len(SCORES) % n_cols > 0))
-    fig = plt.figure(figsize=(n_rows * 10, n_cols * 5))
-    for n, (score, transform_func) in enumerate(SCORES):
+def boxplot_scores_per_best_config(
+    results_df: pd.DataFrame, save_path: Path, split_score_groups: bool = False
+) -> None:
+    if split_score_groups:
+        scores_groups = SCORES_GROUPS
+    else:
+        scores_groups = {"": list(SCORES.keys())}
+    for group_name, group_scores in scores_groups.items():
+        n_cols = 3 if len(group_scores) > 3 else len(group_scores)
+        n_rows = int(len(group_scores) // n_cols + 1 * (len(group_scores) % n_cols > 0))
+        fig = plt.figure(figsize=((n_cols + 1) * 5, n_rows * 5))
+        for n, score in enumerate(group_scores):
+            transform_func = SCORES[score]
+            max_ids = (
+                results_df.groupby(["algo_name", "results_tag"])[
+                    f"mean_test_{score}"
+                ].transform(
+                    transform_func,
+                )
+                == results_df.loc[:, f"mean_test_{score}"]
+            )
+            max_results_df = (
+                results_df.loc[max_ids]
+                .filter(regex=f"(split[0-9]+_test_{score}|algo_name|results_tag)")
+                .melt(id_vars=["algo_name", "results_tag"])
+            )
+
+            ax = fig.add_subplot(n_rows, n_cols, n + 1)
+            sns.boxplot(
+                data=max_results_df,
+                x="algo_name",
+                y=-1 * max_results_df.loc[:, "value"]
+                if score == "davies_bouldin"
+                else "value",
+                hue="results_tag"
+                if max_results_df.loc[:, "results_tag"].unique().shape[0] > 1
+                else None,
+                palette=PALETTE,
+                ax=ax,
+                saturation=1,
+            )
+            box_patches = [patch for patch in ax.patches if type(patch) == PathPatch]
+            lines_per_boxplot = len(ax.lines) // len(box_patches)
+            for i, patch in enumerate(box_patches):
+                face_col = patch.get_facecolor()
+                patch.set_edgecolor(face_col)
+                for line in ax.lines[
+                    i * lines_per_boxplot : (i + 1) * lines_per_boxplot
+                ]:
+                    line.set_color(face_col)
+                    line.set_mfc(face_col)  # facecolor of fliers
+                    line.set_mec(face_col)  # edgecolor of fliers
+
+            ax.set_title(
+                ("-1 * " if score == "davies_bouldin" else "")
+                + SCORES_PRETTY_NAMES[score]
+            )
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+            ax.set_xlabel("")
+            ax.legend([], [], frameon=False)
+        fig.suptitle(
+            f"Results for best hyperparameters for each{' ' + group_name if group_name != '' else ''} score"
+        )
+        plt.legend(
+            loc="lower left",
+            bbox_to_anchor=(1.05, 0.4),
+        )
+        plt.tight_layout()
+        fig.savefig(
+            save_path.parent
+            / f"{save_path.stem}{'_' + group_name if group_name != '' else ''}{save_path.suffix}"
+        )
+
+
+def get_best_results_table(results_df: pd.DataFrame) -> pd.DataFrame:
+    best_results_table = pd.DataFrame()
+    for score, transform_func in SCORES.items():
         max_ids = (
-            results_df.groupby(["algo_name", "results_tag"])[
-                f"mean_test_{score}"
-            ].transform(
+            results_df.groupby(["algo_name"])[f"mean_test_{score}"].transform(
                 transform_func,
             )
             == results_df.loc[:, f"mean_test_{score}"]
         )
         max_results_df = (
-            results_df.loc[max_ids]
-            .filter(regex=f"(split[0-9]+_test_{score}|algo_name|results_tag)")
-            .melt(id_vars=["algo_name", "results_tag"])
+            results_df.loc[max_ids, ["algo_name", f"mean_test_{score}"]]
+            .drop_duplicates()
+            .set_index("algo_name")
         )
-
-        ax = fig.add_subplot(n_rows, n_cols, n + 1)
-        sns.boxplot(
-            data=max_results_df,
-            x="algo_name",
-            y=f"value",
-            hue="results_tag"
-            if max_results_df.loc[:, "results_tag"].unique().shape[0] > 1
-            else None,
-            ax=ax,
-        )
-        ax.set_title(" ".join([s.capitalize() for s in score.split("_")]))
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
-    fig.suptitle("Results for best hyperparameters for each score")
-    plt.tight_layout()
-    fig.savefig(save_path)
+        best_results_table = pd.concat([best_results_table, max_results_df], axis=1)
+    best_results_table.columns = best_results_table.columns.map(SCORES_PRETTY_NAMES)
+    best_results_table = pd.concat(
+        [
+            pd.concat(
+                {group_name: best_results_table.loc[[algo_name]]},
+                names=["algo_group"],
+            )
+            for algo_name, group_name in ALGO_GROUPS.items()
+        ]
+    )
+    best_results_table.index.names = ["", "Algorithm"]
+    return best_results_table
 
 
 def get_best_labels_per_score_per_algo(
@@ -112,7 +257,7 @@ def get_best_labels_per_score_per_algo(
     data_df: pd.DataFrame,
 ) -> dict[str, dict[str, pd.Series]]:
     labels_per_score_per_algo = {}
-    for score, transform_func in tqdm(SCORES):
+    for score, transform_func in tqdm(SCORES.items()):
         labels_per_score_per_algo[score] = {}
         best_ids = (
             results_df.groupby("algo_name")[f"mean_test_{score}"].transform(
@@ -146,7 +291,7 @@ def embedding_plot_per_best_config(
         n_cols = 3
         n_plots = len(labels_per_algo) + 1 * (ground_truth is not None)
         n_rows = int(n_plots // n_cols + 1 * (n_plots % n_cols > 0))
-        fig = plt.figure(figsize=(n_rows * 10, n_cols * 5))
+        fig = plt.figure(figsize=(n_rows * 20, n_cols * 5))
 
         for n, (algo_name, labels) in enumerate(labels_per_algo.items()):
             ax = fig.add_subplot(n_rows, n_cols, n + 1)
@@ -168,7 +313,9 @@ def embedding_plot_per_best_config(
                 ax=ax,
             )
             ax.set_title("Ground truth")
-        fig.suptitle(f"Embedding visualization for best hyperparameters for {score}")
+        fig.suptitle(
+            f"Embedding visualization for best hyperparameters for {SCORES_PRETTY_NAMES[score]}"
+        )
         plt.tight_layout()
         fig.savefig(save_dir / f"Embedding_best_hyperparameters_{score}.png")
 
@@ -178,7 +325,11 @@ def joined_results_dir(results_dirs: list[Path]):
 
 
 def main(
-    results_dirs: list[Path], datasets: list[str], skip_embedding: bool = False
+    results_dirs: list[Path],
+    datasets: list[str],
+    skip_embedding: bool = False,
+    latex_float_precision: int = 3,
+    split_score_groups: bool = False,
 ) -> None:
     for dataset in datasets:
         results_dfs = []
@@ -188,20 +339,30 @@ def main(
                     results_dir / f"{dataset}_grid_search.csv", index_col=0
                 ).reset_index()
             )
-            results_dfs[i]["results_tag"] = results_dir.name
+            results_dfs[i]["results_tag"] = RESULTS_NAMES_DICT.get(
+                results_dir.name, results_dir.name
+            )
         results_df = pd.concat(results_dfs)
 
         output_dir = joined_results_dir(results_dirs)
         output_dir.mkdir(exist_ok=True)
 
+        best_results_table = get_best_results_table(results_df)
+        best_results_table.to_csv(output_dir / f"{dataset}_best_results_table.csv")
+        best_results_table.style.format(precision=latex_float_precision).to_latex(
+            output_dir / f"{dataset}_best_results_table.tex"
+        )
+
         boxplot_mean_scores_per_algo(
             results_df,
-            output_dir / f"{dataset}_mean_score_per_algo.png",
+            output_dir / f"{dataset}_mean_score_per.png",
+            split_score_groups,
         )
 
         boxplot_scores_per_best_config(
             results_df,
             output_dir / f"{dataset}_best_config_scores.png",
+            split_score_groups,
         )
 
         if not skip_embedding and len(results_dirs) == 1:
@@ -270,5 +431,7 @@ if __name__ == "__main__":
         "--datasets", type=lambda x: x.split(","), default=list(DATASETS.keys())
     )
     argparser.add_argument("--skip_embedding", default=False, action="store_true")
+    argparser.add_argument("--latex_float_precision", default=3)
+    argparser.add_argument("--split_score_groups", default=False, action="store_true")
     kwargs = dict(argparser.parse_args()._get_kwargs())
     main(**kwargs)
